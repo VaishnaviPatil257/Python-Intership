89f5182d7390e44a92edabf073bf885c81148786
VaishnaviPatil257 <vaishpatil204@gmail.com>
2026-09-17 16:07:29 +0530

    Add files via upload

---
+++ changing letter case.py
@@ -0,0 +1,16 @@
+text="Digital Dreams Institute"
+print(text.upper())
+print(text.lower())
+print(text.title())
+print("python basics".capitalize())
+
+
+#cleaning and searching text
+print("intern data\n".strip())
+print("Batch-07".replace("Batch","Group"))
+print("welcome to DDI".find("DDI"))
+print("aaa bbb aaa".count("aaa"))
+
+
+#Split and Join-Two Sides of one
+names
